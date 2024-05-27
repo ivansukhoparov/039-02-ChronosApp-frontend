080135a65f1e29ec87c75dd2e30528dfb37137cf
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-27 23:22:41 +0200

    made a little bit

---
+++ src/js/templates.js
@@ -0,0 +1,3 @@
+export const contentArea = document.querySelector("body")
+export const loginFormTemplate = document.querySelector("#login-form-template").content.querySelector(".login")
+export  const errorMessagesTemplate = document.querySelector("#error-template").content.querySelector(".error")
