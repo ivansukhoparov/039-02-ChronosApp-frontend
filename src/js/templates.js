e8c70f7c782a0366a6602172d930dd4983211584
ivansukhoparov <rbcdaaa@gmail.com>
2024-06-08 15:24:03 +0200

    update template items

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,3 +1,13 @@
-export const contentArea = document.querySelector("body")
+
+export const contentArea = document.querySelector("#content-area")
 export const loginFormTemplate = document.querySelector("#login-form-template").content.querySelector(".login")
+export const registrationFormTemplate = document.querySelector("#registration-form-template").content.querySelector(".registration")
 export  const errorMessagesTemplate = document.querySelector("#error-template").content.querySelector(".error")
+export const menuTemplate = document.querySelector("#menu-template").content.querySelector(".menu")
+
+export const taskPreviewTemplate = document.querySelector("#task-preview-template").content.querySelector(".task")
+export const taskExtendedTemplate = document.querySelector("#task-extended-template").content.querySelector(".task")
+
+export const newTaskFormTemplate =document.querySelector("#add-new-task-template").content.querySelector(".new-task")
+
+export const editTaskFormTemplate =document.querySelector("#edit-task-form-template").content.querySelector(".edit-task")
