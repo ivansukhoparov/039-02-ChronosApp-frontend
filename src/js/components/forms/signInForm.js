03f1b8c8a68f56f4f45cd5c035cc79edee427625
ivansukhoparov <rbcdaaa@gmail.com>
2024-06-09 01:40:27 +0200

    add login flow and submit form action for sign in form

--- src/js/components/forms/signInForm.js
+++ src/js/components/forms/signInForm.js
@@ -1,14 +1,47 @@
-import {app, errorPopup} from "../../../index.js";
+import {app, errorPopup, fetchAdapter} from "../../../index.js";
 import {serverResponse} from "../../testData.js";
 import {Menu} from "../menu.js";
 import {BaseFormClass} from "./_baseFormClass.js";
 import {loginFormTemplate} from "../../templates.js";
 
 export class SignInForm extends BaseFormClass {
+    #submitButton = this._frame.querySelector(".submit-button")
 
     constructor() {
         super(loginFormTemplate, app)
-        this.addEvents()
+        this.addSubmitAction()
+    }
+
+    addSubmitAction() {
+        this.#submitButton.addEventListener("click", (evt) => {
+                evt.preventDefault()
+                const formData = this.collectFormValues()
+                this.#submitButton.disabled = true
+                this.login(formData)
+            }
+        )
+    }
+
+    async login(formData) {
+        try {
+            const res = await fetchAdapter.post("/auth/login", formData)
+
+            if (res.accessToken !== undefined) {
+                fetchAdapter.setAccessToken(res.accessToken)
+                const tasks = await fetchAdapter.get("/tasks")
+                console.log("access confirmed")
+                new Menu()
+                app.createTasksEntities(tasks)
+                app.renderTasks()
+            } else {
+                errorPopup.throwErrors(res)
+                this.#submitButton.disabled=false
+            }
+        } catch {
+            errorPopup.throwErrors([{message: "server unavailable", field: "_"}])
+            this.#submitButton.disabled=false
+        }
+
     }
 
     addEvents() {
@@ -21,11 +54,14 @@ export class SignInForm extends BaseFormClass {
         })
     }
 
-    sendRequest(login, password) {
+    async sendRequest(login, password) {
         if (login === "qwe" && password === "qweasd") {
+            const res = await fetchAdapter.get("/tasks")
+
             console.log("access confirmed")
+            console.log(res)
             new Menu()
-            app.createTasksEntities(serverResponse)
+            app.createTasksEntities(res)
             app.renderTasks()
             return {token: "qweasdzxc"}
         } else {
