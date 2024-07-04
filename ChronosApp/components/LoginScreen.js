292c224a33f2591cd5f10e1bc0afa21c49d532cc
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-05 01:26:22 +0200

    create LoginScreen component

---
+++ ChronosApp/components/LoginScreen.js
@@ -0,0 +1,39 @@
+import {StyleSheet, Text, TextInput, View} from "react-native";
+import {useState} from "react";
+import Button from "./Button";
+
+export default function LoginScreen() {
+    const [login, onChangeLogin] = useState('');
+    const [password, onChangePassword] = useState('');
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeLogin}
+                placeholder="Login"
+                value={login}
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangePassword}
+                placeholder="Password"
+                value={password}
+            />
+            <Button label={"Submit"}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems:"center"
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderColor:"#fff",
+        padding: 10,
+        color:"#fff"
+    }
+});
